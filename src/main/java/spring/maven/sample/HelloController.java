18d6ca859b27b4a62b3515f6b46c2b17af92d22f
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-05-31 20:34:45 -0300

    refactor: message of HelloController

--- src/main/java/spring/maven/sample/HelloController.java
+++ src/main/java/spring/maven/sample/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello from Azure Container Apps!";
+        return "Hello world, github actions";
     }
 }
