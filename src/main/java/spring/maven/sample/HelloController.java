b04870c84b794bbf6c312fa98b50c540ba846861
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-04 10:46:43 -0300

    feat: remove scanner quality gate error

--- src/main/java/spring/maven/sample/HelloController.java
+++ src/main/java/spring/maven/sample/HelloController.java
@@ -5,17 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    private String password = "mysecretpassword123";
-    
     @GetMapping("/")
     public String hello() {
-        String message = "Hello World, Java Application!";
-        
-        if(password.equals("mysecretpassword123")) {
-            return message;
-        }
-        
-        return "Access Denied";
+        return "Hello World, Java Application!";
     }
 }
