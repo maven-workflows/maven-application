ff86855d42c2a4f4a000879fe0ab9080df4d528d
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-04 08:49:08 -0300

    feat: adding test in sonar

--- src/main/java/spring/maven/sample/HelloController.java
+++ src/main/java/spring/maven/sample/HelloController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
+    private String password = "mysecretpassword123";
+    
     @GetMapping("/")
     public String hello() {
-        return "Hello World, Sample Application!";
+        String message = "Hello World, Java Application!";
+        
+        if(password.equals("mysecretpassword123")) {
+            return message;
+        }
+        
+        return "Access Denied";
     }
 }
