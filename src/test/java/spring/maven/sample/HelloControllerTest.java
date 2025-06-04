bf4fb24af3c906dd8cd378f7a0e3f8c592c90fd0
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-04 13:58:46 -0300

    feat: adding unit test for coverage

---
+++ src/test/java/spring/maven/sample/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package spring.maven.sample;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void testHello() {
+        HelloController controller = new HelloController();
+        String result = controller.hello();
+        assertEquals("Hello World, Java Application!", result);
+    }
+}
