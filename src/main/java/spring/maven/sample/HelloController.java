dd9d08205b812a3ccc765a9b569ab1b9625e306e
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-04 23:03:04 -0300

    fix: test error

--- src/main/java/spring/maven/sample/HelloController.java
+++ src/main/java/spring/maven/sample/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello World, Java Application";
+        return "Hello World, Java Application!";
     }
 }
