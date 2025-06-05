d21897c943b9709a04454b3046c3232582ad3957
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-04 23:19:35 -0300

    fix: descommented test class

--- src/test/java/spring/maven/sample/HelloControllerTest.java
+++ src/test/java/spring/maven/sample/HelloControllerTest.java
@@ -1,14 +1,14 @@
-// package spring.maven.sample;
+package spring.maven.sample;
 
-// import org.junit.jupiter.api.Test;
-// import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-// class HelloControllerTest {
+class HelloControllerTest {
 
-//     @Test
-//     void testHello() {
-//         HelloController controller = new HelloController();
-//         String result = controller.hello();
-//         assertEquals("Hello World, Java Application!", result);
-//     }
-// }
+    @Test
+    void testHello() {
+        HelloController controller = new HelloController();
+        String result = controller.hello();
+        assertEquals("Hello World, Java Application!", result);
+    }
+}
