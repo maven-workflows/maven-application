2473e116abef2b2ae36fee8f99718d07ed3c38db
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-05 23:51:49 -0300

    test: debug controller

--- src/main/java/spring/maven/sample/HelloController.java
+++ src/main/java/spring/maven/sample/HelloController.java
@@ -3,24 +3,10 @@ package spring.maven.sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class HelloController {
-
     @GetMapping("/")
     public String hello() {
         return "Hello World, Java Application!";
-    }
-
-    @GetMapping("/names")
-    public List<String> getNames() {
-        return Arrays.asList("Ana", "Bruno", "Carla", "Daniel");
-    }
-
-    @GetMapping("/numbers")
-    public List<Integer> getNumbers() {
-        return Arrays.asList(10, 20, 30, 40, 50);
     }
 }
