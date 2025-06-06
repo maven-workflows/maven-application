97328226cc3297e51a97212876895cf9dcfe6b16
Matheus Barros <160808684+Matheusbarros54@users.noreply.github.com>
2025-06-06 08:25:19 -0300

    feat: adding unit test for coverage

--- src/test/java/spring/maven/sample/SampleApplicationTests.java
+++ src/test/java/spring/maven/sample/SampleApplicationTests.java
@@ -3,9 +3,14 @@ package spring.maven.sample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class SampleApplicationTests {
 
+	@Test
+	void main() {
+		SampleApplication.main(new String[] {});
+	}
+	
 	@Test
 	void contextLoads() {
 	}
